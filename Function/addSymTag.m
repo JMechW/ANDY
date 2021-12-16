69b736786a495913b79f993aaa39974d8dabccc3
Jiamin Wang <jmechw@vt.edu>
2021-12-15 19:59:08 -0500

    Delete Function directory

--- Function/addSymTag.m
+++
@@ -1,3 +0,0 @@
-function out1=addSymTag(in1,inTag,inInfo)
-    out1=sym(strcat(char(in1),'_',char(inTag),'_',char(inInfo),'_'),'real');
-end
--- Function/sparseThreshholding.m
+++
@@ -1,5 +0,0 @@
-function outVal=magThreshholding(inVal,inBar)
-
-    outVal=heaviside(abs(inVal)-inBar).*inVal;
-    
-end
