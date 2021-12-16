d6dc2dfac2d3d99113e26d09df4298dbdc5a31e2
Jiamin Wang <jmechw@vt.edu>
2021-12-15 20:40:20 -0500

    Update PathSetup.m

--- Example_SwingBar/PathSetup.m
+++ Example_SwingBar/PathSetup.m
@@ -1,7 +1,7 @@
 clear classes; clear all; close all; clc; restoredefaultpath;
 prjPath=pwd;
 addpath(genpath(prjPath));
-cd('../Library');
+cd('../ANDY');
 libPath=pwd;
 addpath(genpath(libPath));
 cd(prjPath);
